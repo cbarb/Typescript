e804b75c191d76d4943fa1c261ee3410e1c16343
Carlos Barbachano <cbarb035@fiu.edu>
2020-08-30 11:43:18 -0400

    Messing around with types

---
+++ types.js
@@ -0,0 +1,15 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+// let strArr: string[];
+// let boolArr: boolean[];
+var strArr;
+var boolArr;
+myString = 'Hello';
+myNum = 22;
+myBool = true;
+myVar = 'hello';
+strArr = ['hi', 'how', 'are', 'you'];
+boolArr = [true, false, true];
+console.log(strArr);
---
+++ types.ts
@@ -0,0 +1,20 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+let myVar: any;
+
+// let strArr: string[];
+// let boolArr: boolean[];
+
+let strArr: Array<string>;
+let boolArr: Array<boolean>;
+
+myString = 'Hello';
+myNum = 22;
+myBool = true;
+myVar = 'hello';
+
+strArr = ['hi', 'how', 'are', 'you'];
+boolArr = [true, false, true];
+
+console.log(strArr);
